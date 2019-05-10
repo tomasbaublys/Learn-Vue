730d453ce5928e20e4515acc89a63e901f59e4be
vagrant <tomas@forumcinemashe.lt>
2019-05-10 16:25:27 +0000

    Create practical Component exercise 1 - Message.

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
-Vue.component('task-list', {
-	template: '<div><bla v-for="task in tasks">{{ task.task }}</bla></div',
+Vue.component('message', {
+	
+	props: ['title', 'body', 'notes'],
 
 	data() {
 		return {
-			tasks: [
-				{ task: 'Go to store', complete: true },
-				{ task: 'Go to email', complete: false },
-				{ task: 'Go to farm', complete: true },
-				{ task: 'Go to work', complete: false },
-			]
+			isVisible: true
 		};
-	}
-});
+	},
 
-Vue.component('bla', {
-	template: '<li><slot></slot></li>'
+	template: `
+		<div class="alert alert-success" v-show="isVisible">
+			<h4 class="alert-heading">
+				{{ title }}
+				<button type="button" @click="isVisible = false"class="close" aria-label="Close">
+				  <span aria-hidden="true">&times;</span>
+				</button>
+			</h4>
+			<p>
+				{{ body }}
+			</p>
+			<hr>
+			<p class="mb-0">
+				{{ notes }}
+			</p>
+		</div>
+	`,
+
+	// long variant
+	// methods: {
+	// 	hideModal() {
+	// 		this.isVisible = false;
+	// 	}
+	// }
 });
 
 new Vue({
 	el: '#root'
-})
+});
