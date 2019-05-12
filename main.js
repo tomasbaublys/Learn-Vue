8514a4a8140bc94d087c49dce519af7395522f78
vagrant <tomas@forumcinemashe.lt>
2019-05-12 20:59:09 +0000

    Apply Bootstrap Modal Vue script.

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
-Vue.component('message', {
-	
-	props: ['title', 'body', 'notes'],
+Vue.component('modal', {
 
 	data() {
 		return {
-			isVisible: true
+			showModal: false
 		};
 	},
 
 	template: `
-		<div class="alert alert-success" v-show="isVisible">
-			<h4 class="alert-heading">
-				{{ title }}
-				<button type="button" @click="isVisible = false"class="close" aria-label="Close">
-				  <span aria-hidden="true">&times;</span>
-				</button>
-			</h4>
-			<p>
-				{{ body }}
-			</p>
-			<hr>
-			<p class="mb-0">
-				{{ notes }}
-			</p>
-		</div>
+		<div name="modal">
+			<div v-if="! showModal">
+				<button type="button" class="btn btn-primary" @click="openModal">Open Modal</button>
+			</div>
+			<div v-if="showModal">
+				<div class="modal modal-mask" style="display: block">
+				  <div class="modal-dialog" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <h5 class="modal-title">
+					        Modal title
+					    </h5>
+				      	</div>
+				      	<div class="modal-body"></div>
+
+				      	<div class="modal-footer">
+							<button type="button" class="btn btn-outline-info" @click="closeModal()"> Close </button>
+							<button type="button" class="btn btn-primary" data-dismiss="modal" @click="submitAndClose()"> Submit </button>
+				      	</div>
+				    </div>
+				  </div>
+				</div>
+			</div>
+		</div>	
 	`,
 
-	// long variant
-	// methods: {
-	// 	hideModal() {
-	// 		this.isVisible = false;
-	// 	}
-	// }
+	methods: { 
+	   	openModal() { 
+	     	this.showModal = true; 
+	   	},
+	   	closeModal() {
+	   		this.showModal = false;
+	   	},
+	   	submitAndClose() {
+    		this.showModal = false;
+  		} 
+	}
 });
 
 new Vue({
