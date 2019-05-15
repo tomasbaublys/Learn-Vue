0f1e73306782cf48a85000186a8043903116d8f5
vagrant <tomas@forumcinemashe.lt>
2019-05-15 14:33:40 +0000

    Create the common tabs feature from Bumla with vue.js. example.

--- main.js
+++ main.js
@@ -1,49 +1,61 @@
-Vue.component('modal', {
-
-	data() {
-		return {
-			showModal: false
-		};
-	},
-
+Vue.component('tabs', {
 	template: `
-		<div name="modal">
-			<div v-if="! showModal">
-				<button type="button" class="btn btn-primary" @click="openModal">Open Modal</button>
+		<div>
+			<div class="tabs">
+				<ul>
+					<li v-for="tab in tabs" :class="{ 'is-active' : tab.isActive }">
+						<a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+					</li>
+				</ul>
 			</div>
-			<div v-if="showModal">
-				<div class="modal modal-mask" style="display: block">
-				  <div class="modal-dialog" role="document">
-				    <div class="modal-content">
-				      <div class="modal-header">
-				        <h5 lass="modal-title">
-							<slot name="title"></slot>
-					    </h5>
-				      </div>
-				      <div class="modal-body">
-							<slot name="body"></slot>
-						</div>
-				      	<div class="modal-footer">
-							<button type="button" class="btn btn-outline-info" @click="closeModal()"> Close </button>
-							<button type="button" class="btn btn-primary" data-dismiss="modal" @click="submitAndClose()"> Submit </button>
-				      	</div>
-				    </div>
-				  </div>
-				</div>
+
+			<div class="tabs-details">
+				<slot></slot>
 			</div>
 		</div>	
 	`,
 
-	methods: { 
-	   	openModal() { 
-	     	this.showModal = true; 
-	   	},
-	   	closeModal() {
-	   		this.showModal = false;
-	   	},
-	   	submitAndClose() {
-    		this.showModal = false;
-  		} 
+	data() {
+		return { tabs: [] };
+	},
+
+	created() {
+		this.tabs = this.$children;
+	},
+
+	methods: {
+		selectTab(selectedTab) {
+			this.tabs.forEach(tab => {
+				tab.isActive = (tab.name == selectedTab.name);
+			});
+		}
+	}
+});
+
+Vue.component('tab', {
+	template:`
+		<div v-show="isActive"><slot></slot></div>
+	`,
+
+	props: {
+		name: { required: true },
+		selected: { default: false }
+	},
+
+	computed: {
+		href() {
+			return '#' + this.name.toLowerCase().replace(/ /g, '-');
+		}
+	},
+
+	data() {
+		return {
+			isActive: false
+		};
+	},
+
+	mounted() {
+		this.isActive = this.selected;
 	}
 });
 
