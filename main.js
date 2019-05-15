6600b355a08c1c31bcc4899dc00ba83058e7a739
vagrant <tomas@forumcinemashe.lt>
2019-05-15 16:11:19 +0000

    Create example with Named Slots in a nutshell and add modal functionality with Bulma.

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
-window.Event = new Vue();
-
-Vue.component('coupon', {
+Vue.component('modal', {
 	template: `
-		<input placeholder="Enter your coupon code" @blur="onCouponApplied">
-	`,
-	methods: {
-		onCouponApplied() {
-			Event.$emit('applied');
-		}
-	}
+		<div class="modal is-active">
+		  <div class="modal-background"></div>
+
+		  <div class="modal-card">
+		    <header class="modal-card-head">
+		      <p class="modal-card-title">
+				<slot name="header"></slot>
+		      </p>
+
+		      <button class="delete" aria-label="close" @click="$emit('close')"></button>
+		    </header>
+		    <section class="modal-card-body">
+		      <slot></slot>
+		    </section>
+		    <footer class="modal-card-foot">
+				<slot name="footer"></slot>
+		    </footer>
+		  </div>
+
+		</div>
+	`
 });
 
 new Vue({
 	el: '#root',
 
 	data: {
-		onCouponApplied: false
-	},
-
-	created() {
-		Event.$on('applied', () => alert('Handling it!'))
+		showModal: false
 	}
 });
